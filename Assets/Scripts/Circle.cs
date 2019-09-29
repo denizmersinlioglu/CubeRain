0285ead8cf96887b8887ff6dc77d00bd0de5e5df
Deniz <mersinliogludeniz@gmail.com>
2019-09-29 18:07:13 +0300

    Initial Commit

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Circle : MonoBehaviour {
+
+    public int VertexCount = 40;
+    public float LineWidth = 0.01f;
+    public float Radius;
+    public bool CircleFillScreen;
+
+    private LineRenderer lineRenderer;
+
+
+    private void Awake() {
+        lineRenderer = GetComponent<LineRenderer>();
+        SetupCircle();
+    }
+
+    private void SetupCircle() {
+
+        if (CircleFillScreen) {
+            float yMax = Camera.main.pixelRect.yMax;
+            float yMin = Camera.main.pixelRect.yMin;
+            Vector3 maxPoint = Camera.main.ScreenToWorldPoint(new Vector3(0f, yMax, 0f));
+            Vector3 minPoint = Camera.main.ScreenToWorldPoint(new Vector3(0f, yMin, 0f));
+            Radius = Vector3.Distance(maxPoint, minPoint)/2;
+        }
+
+        float deltaTheta = (2f * Mathf.PI) / (float)VertexCount;
+        float theta = 0f;
+
+        lineRenderer.positionCount = VertexCount;
+
+        for (int i = 0; i < lineRenderer.positionCount; i++) {
+            Vector3 pos = Radius * new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0f);
+            lineRenderer.SetPosition(i, pos);
+            theta += deltaTheta;
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos() {
+        float deltaTheta = (2f * Mathf.PI) / (float) VertexCount;
+        float theta = 0f;
+
+        Vector3 oldPos = Vector3.zero;
+
+        for (int i = 0; i < VertexCount + 1; i++) {
+            Vector3 pos = Radius * new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0f);
+            Vector3 newPos = transform.position + pos;
+            Gizmos.DrawLine(oldPos, newPos);
+            oldPos = newPos;
+
+            theta += deltaTheta;
+        }
+    }
+
+#endif
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class TouchController : MonoBehaviour {
+    // Start is called before the first frame update
+
+    CompositeDisposable disposables;
+    private float TopMargin = 150;
+
+
+
+    void Start() {
+        disposables = new CompositeDisposable();
+        SetupSubscriptions();
+    }
+
+    private void SetupSubscriptions() {
+        Debug.Log("Touch Controller Setup Subscriptions");
+
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetMouseButtonDown(0))
+            .SelectMany(_ => gameObject.UpdateAsObservable())
+            .TakeUntil(this.UpdateAsObservable().Where(_ => Input.GetMouseButtonUp(0)))
+            .Select(_ => Input.mousePosition)
+            .RepeatUntilDestroy(this)
+            .Subscribe(
+                pos => {
+                    Debug.Log(string.Format("{0} : {1}", pos.x, pos.y));
+
+                })
+            .AddTo(disposables);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
