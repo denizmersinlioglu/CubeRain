70beb366d1c05529d9c42ce20b189f4ceb8465f1
Deniz <mersinliogludeniz@gmail.com>
2019-09-29 18:39:01 +0300

    Line and Circle Refactored According to Responsibilities

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -3,50 +3,93 @@
 [RequireComponent(typeof(LineRenderer))]
 public class Circle : MonoBehaviour {
 
-    public int VertexCount = 40;
-    public float LineWidth = 0.01f;
-    public float Radius;
-    public bool CircleFillScreen;
+    public int vertexCount = 40;
+    public float lineWidth = 0.01f;
+    public float radius;
+    public bool circleFillScreen;
 
     private LineRenderer lineRenderer;
+    public Transform leftCog;
+    public Transform rightCog;
 
+
+    #region LifeCycle
 
     private void Awake() {
         lineRenderer = GetComponent<LineRenderer>();
         SetupCircle();
     }
 
+    private void Start() {
+        leftCog.position = new Vector3(-radius, 0f, 0f);
+        rightCog.position = new Vector3(radius, 0f, 0f);
+    }
+
+    private void Update() {
+        if (Input.GetMouseButton(0)) {
+            Vector3 leftPos = PlaceOnCircle(Input.mousePosition);
+            leftCog.position = leftPos;
+        }
+
+        if (Input.GetMouseButton(1)) {
+            Vector3 rightPos = PlaceOnCircle(Input.mousePosition);
+            rightCog.position = rightPos;
+        }
+    }
+
+    #endregion
+
+    #region Setup Methods
+
+    private Vector3 PlaceOnCircle(Vector3 position) {
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        Vector3 pos = ray.GetPoint(0);
+
+        pos = transform.InverseTransformDirection(pos);
+        float angle = Mathf.Atan2(pos.x, pos.y) * Mathf.Rad2Deg;
+        pos.x = radius * Mathf.Sin(angle * Mathf.Deg2Rad);
+        pos.y = radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        pos.z = 0f;
+
+        return pos;
+
+    }
+
     private void SetupCircle() {
 
-        if (CircleFillScreen) {
+        if (circleFillScreen) {
             float yMax = Camera.main.pixelRect.yMax;
             float yMin = Camera.main.pixelRect.yMin;
             Vector3 maxPoint = Camera.main.ScreenToWorldPoint(new Vector3(0f, yMax, 0f));
             Vector3 minPoint = Camera.main.ScreenToWorldPoint(new Vector3(0f, yMin, 0f));
-            Radius = Vector3.Distance(maxPoint, minPoint)/2;
+            radius = Vector3.Distance(maxPoint, minPoint)/2;
         }
 
-        float deltaTheta = (2f * Mathf.PI) / (float)VertexCount;
+        float deltaTheta = (2f * Mathf.PI) / (float)vertexCount;
         float theta = 0f;
 
-        lineRenderer.positionCount = VertexCount;
+        lineRenderer.positionCount = vertexCount;
 
         for (int i = 0; i < lineRenderer.positionCount; i++) {
-            Vector3 pos = Radius * new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0f);
+            Vector3 pos = radius * new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0f);
             lineRenderer.SetPosition(i, pos);
             theta += deltaTheta;
         }
     }
 
+    #endregion
+
+    #region GUI Methods
+
 #if UNITY_EDITOR
     private void OnDrawGizmos() {
-        float deltaTheta = (2f * Mathf.PI) / (float) VertexCount;
+        float deltaTheta = (2f * Mathf.PI) / (float) vertexCount;
         float theta = 0f;
 
         Vector3 oldPos = Vector3.zero;
 
-        for (int i = 0; i < VertexCount + 1; i++) {
-            Vector3 pos = Radius * new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0f);
+        for (int i = 0; i < vertexCount + 1; i++) {
+            Vector3 pos = radius * new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0f);
             Vector3 newPos = transform.position + pos;
             Gizmos.DrawLine(oldPos, newPos);
             oldPos = newPos;
@@ -56,12 +99,7 @@ public class Circle : MonoBehaviour {
     }
 
 #endif
-    void Start() {
 
-    }
+    #endregion
 
-    // Update is called once per frame
-    void Update() {
-
-    }
 }
--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Line : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Line : MonoBehaviour {
+    public float lineWidth = 0.2f;
+    public string sortingLayer;
+    
+    private LineRenderer lineRenderer;
+
+    public Transform leftEnd;
+    public Transform rightEnd;
+
+    private void Start() {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, leftEnd.position);
+        lineRenderer.SetPosition(1, rightEnd.position);
+        lineRenderer.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition(0, leftEnd.position);
+        lineRenderer.SetPosition(1, rightEnd.position);
     }
 }
